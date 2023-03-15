3ee9c6ecf3f2055da74bc5e1b969ef20f0d286a6
eugeniusms <eugeniusms@gmail.com>
2023-03-15 14:06:46 +0700

    #1 Tetap Commit Walaupun Bingung

--- src/main/java/id/ac/ui/cs/advprog/tutorial5/service/medicine/MedicineServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/tutorial5/service/medicine/MedicineServiceImpl.java
@@ -14,36 +14,71 @@ public class MedicineServiceImpl implements MedicineService {
     private final MedicineRepository medicineRepository;
     @Override
     public List<Medicine> findAll() {
-        // TODO: Lengkapi kode berikut
-        return null;
+        // TODO: DONE
+        // Lengkapi kode berikut
+        List<Medicine> medicines = null;
+        medicines = medicineRepository.findAll();
+        return medicines;
     }
 
     @Override
     public Medicine findById(Integer id) {
-        // TODO: Lengkapi kode berikut (Pastikan Anda memanfaatkan Exceptions yang ada!)
-        return null;
+        // TODO: DONE
+        // Lengkapi kode berikut (Pastikan Anda memanfaatkan Exceptions yang ada!)
+        if (isMedicineDoesNotExist(id)) {
+            return null;
+        }
+        Medicine medicine = null;
+        medicine = medicineRepository.findById(id).get();
+        return medicine;
     }
 
     @Override
     public Medicine create(MedicineRequest request) {
-        // TODO: Lengkapi kode berikut
+        // TODO: DONE
+        // Lengkapi kode berikut
         Medicine medicine = null;
+        medicine = Medicine.builder()
+                .name(request.getName())
+                .category(request.getCategory())
+                .dose(request.getDose())
+                .stock(request.getStock())
+                .price(request.getPrice())
+                .manufacturer(request.getManufacturer())
+                .build();
+
         return medicineRepository.save(medicine);
     }
 
     @Override
     public Medicine update(Integer id, MedicineRequest request) {
-        // TODO: Lengkapi kode berikut (Pastikan Anda memanfaatkan Exceptions yang ada!)
+        // TODO: DONE
+        // Lengkapi kode berikut (Pastikan Anda memanfaatkan Exceptions yang ada!)
         if (isMedicineDoesNotExist(id)) {
             return null;
         }
         Medicine medicine = null;
+        medicine = medicineRepository.findById(id).get();
+
+        medicine.setId(id);
+        medicine.setName(request.getName());
+//        medicine.setCategory(request.getCategory());
+        medicine.setDose(request.getDose());
+        medicine.setPrice(request.getPrice());
+        medicine.setStock(request.getStock());
+        medicine.setManufacturer(request.getManufacturer());
+
         return this.medicineRepository.save(medicine);
     }
 
     @Override
     public void delete(Integer id) {
-        // TODO: Lengkapi kode berikut (Pastikan Anda memanfaatkan Exceptions yang ada!)
+        // TODO: DONE
+        // Lengkapi kode berikut (Pastikan Anda memanfaatkan Exceptions yang ada!)
+        if (isMedicineDoesNotExist(id)) {
+            return;
+        }
+        medicineRepository.deleteById(id);
     }
 
     private boolean isMedicineDoesNotExist(Integer id) {
